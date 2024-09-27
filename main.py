80993e5fa29b4a453a973f48154689b727fc1a99
Marcello Brianzoli <54253066+nemesi-ita@users.noreply.github.com>
2024-09-27 11:34:34 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,45 +1,16 @@
 # PYWIFI docu. --> https://github.com/awkman/pywifi/blob/master/DOC.md
 # https://github.com/ifindev/indoor-positioning-algorithms
 
-
-import socket
-import re
 import pywifi
 from time import sleep
 import argparse
 import os.path
 import sys
 import subprocess
-import math
+import serial
 
 network_signals = {}    # dizionario che contiene {"ssid": rssi_max}
-network_positions = {}  # dizionario che contiene {"ssid": (posizione, distanza_minima)}
-
-
-# Trovo le corrispondenze gps
-def parse_nmea(data):
-    gprmc_match = re.search(r'\$GPRMC,\d+\.\d+,[AV],(\d+\.\d+),([NS])(\d+\.\d+),([EW])(\d+\.\d+)', data)
-    gpgga_match = re.search(r'\$GPGGA,\d+\.\d+,\d+\.\d+,[NS],\d+\.\d+,[EW],', data)
-    
-    if gprmc_match:
-        latitude = float(gprmc_match.group(1))
-        if gprmc_match.group(2) == 'S':
-            latitude *= -1
-        longitude = float(gprmc_match.group(3))
-        if gprmc_match.group(4) == 'W':
-            longitude *= -1
-        return latitude, longitude
-    elif gpgga_match:
-        parts = data.split(',')
-        latitude = float(parts[2])
-        if parts[3] == 'S':
-            latitude *= -1
-        longitude = float(parts[4])
-        if parts[5] == 'W':
-            longitude *= -1
-        return latitude, longitude
-    else:
-        return None
+network_positions = {}  # dizionario che contiene {"ssid": ((latitude, longitude), minimum_distance)}
 
 # Avvio l'interfaccia corretta
 def start_WIFI():
@@ -71,6 +42,17 @@ def start_WIFI():
 
     return wifi_interface
 
+def start_GPS():
+    # Configura la porta seriale (cambia con la tua porta o '/dev/ttyUSBX' su Linux)
+    try:
+        ser = serial.Serial('COM7', 9600, timeout=1)
+        return ser
+    except FileNotFoundError:
+        print("Cannot find any NMEA device!!!") # Aggiungere
+        exit(0)
+    print("Stabilize connection between NMEA device and your device...")
+    sleep(2)  # Attendi che la connessione seriale si stabilizzi
+
 
 def distance_calc(rssi, tx_power, attenuazione):
     # Calcola la distanza utilizzando la formula di calcolo della distanza RSSI
@@ -85,13 +67,39 @@ def distance_calc(rssi, tx_power, attenuazione):
     distanza = 10 ** ((tx_power - rssi) / (20 * attenuazione))
     return distanza
 
+def getPos():
+    try:
+        ser_iface.flush()
+        lat = lon = alt = vel = sat = 0
+        while True:
+            sleep(0.5)
+            if ser_iface.in_waiting > 0:  # Verifica se ci sono dati disponibili
+                line = ser_iface.readline().decode().rstrip()  # Leggi e decodifica la linea NMEA
+                
+                if line.startswith('Lat '):
+                    lat = line.split(' ')[1]
+                elif line.startswith('Lon '):
+                    lon = line.split(' ')[1]
+                elif line.startswith('Alt '):
+                    alt = line.split(' ')[1]
+                elif line.startswith('Vel '):
+                    vel = line.split(' ')[1]
+                elif line.startswith('Sat '):
+                    sat = line.split(' ')[1]
+                elif line.startswith("END"):
+                    return lat, lon, alt, vel, sat
+                else:
+                    continue
+    except Exception as e:
+        print(e)
 
 # Avvio la ricerca dei wifi
-def scan_wifi(iface, s):
+def scan_wifi(iface):
     attenuazione = 2
     iface.scan()  # Start scanning for WiFi networks
-    scan_results = iface.scan_results()
     sleep(2)
+    scan_results = iface.scan_results()
+    
     
     if not scan_results:
         print("Nessuna rete trovata...")
@@ -99,8 +107,8 @@ def scan_wifi(iface, s):
     
     print("WiFi networks found:")
 
-    # sistemare ricezione di NMEA
     # capire se esiste un metodo per rssi e potenza
+    latitude, longitude, altitude, velocity, satellites = getPos()
     for network in scan_results:
         ssid = network.ssid
         rssi =  network.signal
@@ -110,16 +118,15 @@ def scan_wifi(iface, s):
         if network_signals.get(ssid) is None or rssi > network_signals.get(ssid)[0]:
             network_signals.update({ssid: (rssi, potenza)}) # aggiorno le potenze
 
-        parsed_position = None
-        while parsed_position is None:
-            data, addr = s.recvfrom(2048)
-            parsed_position = parse_nmea(data.decode())
-        network_positions.update({ssid: (parsed_position, distance)})
+        network_positions.update({ssid: ((latitude, longitude), distance)})
 
-        print(f'''RETE --> {ssid}
-              Potenza alle coordinate {network_positions.get(ssid)[0]} --> {network_signals.get(ssid)[0]}
-              Distanza approssimata --> {network_positions.get(ssid)[1]}''')
-        sleep(0.5)
+        print(f'''NETWORK --> {ssid}
+              Signal strength at coordinates {network_positions.get(ssid)[0]} --> {network_signals.get(ssid)[0]}
+              Distance --> {network_positions.get(ssid)[1]}''')
+
+# Configurazione interfacce
+iface = start_WIFI()
+ser_iface = start_GPS()
 
 def init():
     print('''
@@ -148,27 +155,6 @@ def init():
                   '`--._,dd###pp=""'
     ''')
 
-    # Funzione di validazione per la porta
-    def validate_port(port):
-        if port is None:
-            raise argparse.ArgumentTypeError(f"Porta non configurata.")
-        if not 0 <= port <= 65535:
-            raise argparse.ArgumentTypeError(f"La porta deve essere compresa tra 0 e 65535.")
-        return port
-
-    # Funzione di validazione per l'IP
-    def validate_ip(ip):
-        if ip is None:
-            raise argparse.ArgumentTypeError(f"IP non configurato.")
-
-        chunks = ip.split('.')
-        if len(chunks) != 4:
-            raise argparse.ArgumentTypeError("L'IP deve avere 4 chunk separati da punti.")
-        for chunk in chunks:
-            if not 0 <= int(chunk) <= 255:
-                raise argparse.ArgumentTypeError(f"Chunk di IP non valido: *{chunk}* \nDeve essere compreso tra 0 e 255.")
-        return ip
-
     # Funzione di validazione per l'estensione
     def validate_extension(extension):
         if extension is None:
@@ -180,17 +166,12 @@ def init():
 
 
     # Creazione e gestione argomenti da linea di comando
-    parser = argparse.ArgumentParser(usage=f"python3 {os.path.basename(sys.argv[0])} [-h] [-p PORT] [-a ADDRESS] [-e EXTENSION]", 
+    parser = argparse.ArgumentParser(usage=f"python3 {os.path.basename(sys.argv[0])} [-h] [-a ADDRESS] [-e EXTENSION]", 
                                     description='Visualizza e traccia le reti wifi nella tua zona con coordinate GPS e client connessi! ;)', 
                                     add_help=False,
-                                    epilog="Il telefono/trasmettitore NMEA e dispositivo di wardriving devono essere sulla stessa rete!")
+                                    epilog="Ensure the serial port is correct!")
 
-    parser.add_argument('-h', '--help', action='help', default=argparse.SUPPRESS, help="Mostra questo messaggio di aiuto")
-    parser.add_argument('-p', '--port', nargs="?", default=2947, type=int, metavar="PORT", dest="port", 
-                        help='Porta di ricezione delle coordinate GPS. Default: 2947')
-
-    parser.add_argument('-a', '--address', nargs="?", default='0.0.0.0', type=str, metavar="IP", dest="address", 
-                        help='Indirizzo IP di ricezione. Default: 0.0.0.0')
+    parser.add_argument('-h', '--help', action='help', default=argparse.SUPPRESS, help="Show this help message")
 
     parser.add_argument('-e', '--export', nargs="?", default='csv', type=str, metavar="EXTENSION", dest="export", 
                         help='Seleziona il formato di esportazione. Default .CSV')
@@ -198,44 +179,17 @@ def init():
     
     try:
         args = parser.parse_args()
-        validate_port(args.port)
-        validate_ip(args.address)
         validate_extension(args.export)
-
-        UDP_IP = args.address
-        UDP_PORT = args.port
-
-        # Configura il socket UDP
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.bind((UDP_IP, UDP_PORT))
+        
     except Exception as e:
         print(f"\n\n\t\t**ERRORE**\n\n{e}")
         return 1
-        
-        
-    # Configurazione interfaccia wireless
-    iface = start_WIFI()
-    print(f"In ascolto sul socket: {args.address}:{args.port}\nIn attesa di dati da GPSDForwarder...")
-
+    
+    print("Waiting NMEA data...")  
     while True:
-        scan_wifi(iface, sock)
-        '''
-        try:
-            # Ricevi i dati GPS (ASYNC)
-            data, addr = sock.recvfrom(2048)  
-            parsed_data = parse_nmea(data.decode())
-
-            if parsed_data:
-                scan_wifi(iface)
-                latitude, longitude = parsed_data
-                print("Latitudine:", latitude)
-                print("Longitudine:", longitude)
-                
-        except KeyboardInterrupt:
-            print("Saving and exit!")
-            #exportData()
-            break
-        '''
+        scan_wifi(iface)
+        sleep(0.2)
+        
 
 if __name__ == "__main__":
     try:
@@ -248,4 +202,7 @@ if __name__ == "__main__":
 / (_..-' // (< _     ;_..__               ; `' / ///
  / // // //  `-._,_)' // / ``--...____..-' /// / //
         ''')
-    print("\n\n\t\topenWD by #N3m3s1#")
+    finally:
+        ser_iface.close()
+    
+    print("\n\n\t\t openWD by #NEMESI-ITA#")
